51662d95b81da93188f1cc6c5d861074186dc2be
Vajahath Ahmed <vajuoff.1@gmail.com>
2026-03-08 19:31:15 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+declare const M: any;
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".parallax");
+  M.Parallax.init(elems, {});
+});
+
+// Find the right method, call on correct element
+function launchFullScreen(element: any) {
+  if (element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+
+// Launch fullscreen for browsers that support it!
+addEventListener("touchmove", () => {
+  launchFullScreen(document.documentElement); // the whole pagedocument.documentElement
+});
